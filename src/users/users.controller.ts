b18d5f043838e7abc3ad330da2a14495bb5f7fed
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-26 18:19:17 -0300

    :sparkles: feat: creating POST route for 'Users'

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { GetUsersDto } from './dtos/get-users.dto';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
   public getUsers(@Param() getUsersDto: GetUsersDto) {
     console.log(getUsersDto);
+    return 'You sent a GET request to users endpoint';
+  }
+
+  @Post()
+  public postUser(@Body() postUserDto: CreateUserDto) {
+    console.log(postUserDto);
     return 'You sent a GET request to users endpoint';
   }
 }
