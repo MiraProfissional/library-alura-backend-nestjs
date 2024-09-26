9d9927f5191c0e6b5690495d99caabc07724374c
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-26 18:17:01 -0300

    :sparkles: feat: creating CreateUserDto

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(100)
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+    message:
+      'No minimo 8 caracteres, no minimo uma letra, um número e um caracter especial',
+  })
+  password: string;
+}
