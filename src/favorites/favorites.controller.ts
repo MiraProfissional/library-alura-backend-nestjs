57ccecc00e33f7e4ba3b2aa96a3eda35e8a9ac3c
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-27 19:34:43 -0300

    :sparkles: feat: creating DELETE route for 'Favorites'

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { GetUsersDto } from '../users/dtos/get-users.dto';
 
 @Controller('favorites')
@@ -17,5 +24,11 @@ export class FavoritesController {
     console.log(idBook);
     console.log(idUser);
     return 'You sent a POST request to favorites endpoint';
+  }
+
+  @Delete('/:id')
+  public removeFavorite(@Param('id', ParseIntPipe) idBook: number) {
+    console.log(idBook);
+    return 'You sent a DELETE request to favorites endpoint';
   }
 }
