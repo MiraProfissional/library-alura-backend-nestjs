0640d8935ece20bd9a3b1a3cf8b3956d4732f17c
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-28 00:00:16 -0300

    :sparkles: feat: creating GET route for 'Books'

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { GetUsersDto } from 'src/users/dtos/get-users.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  @Get('/:id?')
+  public getBooks(@Param() getBooksDto: GetUsersDto) {
+    console.log(getBooksDto);
+    return 'You sent a GET request to books endpoint';
+  }
+}
