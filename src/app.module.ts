d88566c712d0e3667d5ae6d9b44cdb9ee72e4324
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-27 23:47:46 -0300

    :sparkles: feat: creating 'Books' module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [UsersModule, FavoritesModule],
+  imports: [UsersModule, FavoritesModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
