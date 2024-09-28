23751b325be4067b53c41dd11531f219e6dbe62b
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-28 09:44:46 -0300

    :sparkles: feat: creating a Enum for genre

---
+++ src/books/dtos/genre-books.enum.ts
@@ -0,0 +1,7 @@
+export enum Genre {
+  PROGRAMMING_LANGUAGE = 'Linguagem de Programação',
+  LEADERSHIP = 'Liderança',
+  ALGORITHMS = 'Algoritmos',
+  OOP = 'Programação Orientada a Objetos',
+  LOGIC_PROGRAMMING = 'Lógica de Programação',
+}
